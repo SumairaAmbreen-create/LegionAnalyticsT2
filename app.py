bd8fc7d901978a31cb8637a395b09dd9c2bd13eb
Sumaira Ambreen <sumaira@cs.qau.edu.pk>
2025-04-18 21:21:30 +0500

    Re-add cleaned dataset

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="Vehicle Listings Dashboard", layout="wide")
 
 # Load and clean data
 try:
-    df = pd.read_csv("vehicles_us.csv", sep=",", encoding='utf-8')
+    df = pd.read_csv("vehicles_us1.csv", sep=",", encoding='utf-8')
     st.success("CSV loaded successfully!")
     st.dataframe(df.head())
 
