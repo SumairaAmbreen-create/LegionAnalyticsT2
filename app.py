bc32cf11199a80ac96a4c36720f0f0326b6eb588
Sumaira Ambreen <sumaira@cs.qau.edu.pk>
2025-04-20 12:13:29 +0500

    Update EDA.ipynb and app.py files

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ st.set_page_config(page_title="Vehicle Listings Dashboard", layout="wide")
 
 # Load cleaned data
 try:
-    df = pd.read_csv("vehicles_cleaned.csv", encoding='utf-8')
+    # df = pd.read_csv("vehicles_cleaned.csv", encoding='utf-8')
+    df = pd.read_csv("vehicles_cleaned.csv", encoding='utf-8', errors='ignore')
     st.dataframe(df.head())
 
 except Exception as e:
